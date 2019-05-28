bade80682c9ba35ce76bc3d7d94071e2bee28ec2
Sho Matsuzaki <sho.g.matsuzaki@gmail.com>
2019-05-28 16:30:49 -0400

    Fixed bug for filtering on project detail pages.

--- js/main.js
+++ js/main.js
@@ -98,11 +98,13 @@ function myRemoveClass(element, name) {
 
 // Add active class to the button that has just been clicked
 var btnContainer = document.getElementById("btncontainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+if (btnContainer) {
+  var btns = btnContainer.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+}
 }
